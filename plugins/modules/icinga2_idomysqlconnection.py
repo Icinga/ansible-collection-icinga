2c161097aecafc63f1286b6037f4b3ec81a2fbeb
Lennart Betz <lennart.betz@netways.de>
2021-03-31 11:08:51 +0200

    add feature idomysql

---
+++ plugins/modules/icinga2_idomysqlconnection.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+  
+
+from ansible.module_utils.basic import AnsibleModule
+
+def main():
+	module = AnsibleModule(
+		argument_spec = dict(
+			state                = dict(default='present', choices=['present', 'absent']),
+			name                 = dict(required=True),
+			order                = dict(default=10, type='int'),
+			file                 = dict(default='features-available/ido-mysql.conf', type='str'),
+			host                 = dict(default='localhost', type='str'),
+			port                 = dict(type='int'),
+			socket_path          = dict(type='str'),
+			user                 = dict(default='icinga2', type='str'),
+			password             = dict(required=True, type='str'),
+			database             = dict(default='icinga2', type='str'),
+			enable_ssl           = dict(type='bool'),
+			ssl_key              = dict(type='str'),
+			ssl_cert             = dict(type='str'),
+			ssl_ca               = dict(type='str'),
+			ssl_capath           = dict(type='str'),
+			ssl_cipher           = dict(type='str'),
+			table_prefix         = dict(type='str'),
+			instance_name        = dict(type='str'),
+			instance_description = dict(type='str'),
+			enable_ha            = dict(type='bool'),
+			failover_timeout     = dict(type='str'),
+			cleanup              = dict(type='dict'),
+			categories           = dict(type='list', elements='str'),
+		)
+	)
+
+	args = module.params
+	name = args.pop('name')
+	order = args.pop('order')
+	state = args.pop('state')
+	file = args.pop('file')
+
+	module.exit_json(changed=False, args=args, name=name, order=str(order), state=state, file=file)
+
+if __name__ == '__main__':
+	main()
+

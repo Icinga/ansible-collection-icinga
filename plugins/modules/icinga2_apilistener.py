7da249f98bcb12f1488524a392d7bd3f5d1bc7a8
Thilo Wening <thilo.wening@netways.de>
2021-04-12 08:55:16 +0200

    fixed indent in argument list

--- plugins/modules/icinga2_apilistener.py
+++ plugins/modules/icinga2_apilistener.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-  
+
 
 from ansible.module_utils.basic import AnsibleModule
 
@@ -8,7 +8,7 @@ def main():
 		argument_spec = dict(
 			state                       = dict(default='present', choices=['present', 'absent']),
 			name                        = dict(required=True),
-			order		            = dict(default=10, type='int'),
+			order		                = dict(default=10, type='int'),
 			file                        = dict(default='features-available/api.conf', type='str'),
 			ticket_salt                 = dict(default='TicketSalt', type='str'),
 			bind_host                   = dict(type='str'),
@@ -34,4 +34,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-

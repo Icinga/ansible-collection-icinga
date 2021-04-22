c23a954095d6aa427bf02dd97703330b61370dae
Thilo W <mkayontour@gmail.com>
2021-04-22 17:07:13 +0200

    added object TimePeriod ref #27

---
+++ plugins/modules/icinga2_timeperiod.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+
+from ansible.module_utils.basic import AnsibleModule
+
+def main():
+	module = AnsibleModule(
+		argument_spec = dict(
+			state           = dict(default='present', choices=['present', 'absent']),
+			name            = dict(required=True),
+			order		    = dict(default=10, type='int'),
+			file            = dict(required=True, type='str'),
+			display_name    = dict(type='str'),
+			ranges          = dict(type='dict'),
+			prefer_includes = dict(type='bool'),
+			excludes        = dict(type='list', elements='str'),
+			includes        = dict(type='list', elements='str')
+		)
+	)
+
+	args = module.params
+	name = args.pop('name')
+	order = args.pop('order')
+	state = args.pop('state')
+	file = args.pop('file')
+
+	module.exit_json(changed=False, args=args, name=name, order=str(order), state=state, file=file)
+
+if __name__ == '__main__':
+	main()

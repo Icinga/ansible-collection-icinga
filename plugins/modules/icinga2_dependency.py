6fb61854b0d0ac1c341ca998dbc341e9edd03182
Afeef Ghannam <afeef.ghannam@netways.de>
2021-09-09 09:58:57 +0200

    * Remove the forcing of parameter setting temp. or obj * Enable apply rules

--- plugins/modules/icinga2_dependency.py
+++ plugins/modules/icinga2_dependency.py
@@ -14,15 +14,17 @@ def main():
             imports               = dict(default=list(), type='list', elements='str'),
             apply                 = dict(default=False, type='bool'),
             apply_target          = dict(type='str'),
-            parent_host_name      = dict(required=True, type='str'),
+            parent_host_name      = dict(type='str'),
             parent_service_name   = dict(type='str'),
-            child_host_name       = dict(required=True, type='str'),
+            child_host_name       = dict(type='str'),
             child_service_name    = dict(type='str'),
             disable_checks        = dict(type='bool'),
             disable_notifications = dict(type='bool'),
             ignore_soft_states    = dict(type='bool'),
             period                = dict(type='str'),
             states                = dict(type='list', elements='str'),
+            assign                = dict(default=list(), type='list', elements='str'),
+			ignore                = dict(default=list(), type='list', elements='str'),
         )
     )
 

59a28dead58f1ba0c722b4b95389dd73b2bff3a2
Afeef Ghannam <afeef.ghannam@netways.de>
2021-09-10 09:36:31 +0200

    Correct the feauture file path

--- plugins/modules/icinga2_icingadb.py
+++ plugins/modules/icinga2_icingadb.py
@@ -10,7 +10,7 @@ def main():
 			state                = dict(default='present', choices=['present', 'absent']),
 			name                 = dict(required=True),
 			order                = dict(default=10, type='int'),
-			file                 = dict(default='features-available/gelf.conf', type='str'),
+			file                 = dict(default='features-available/icingadb.conf', type='str'),
 			host                 = dict(type='str'),
 			port                 = dict(type='int'),
 			path                 = dict(type='str'),

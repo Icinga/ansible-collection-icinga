227294a242fbd2623d9df64587db807ac2d5d8a8
Thilo W <mkayontour@gmail.com>
2021-11-11 13:09:47 +0100

    updated repos and feature tests

--- molecule/default/tests/integration/test_repos.py
+++ molecule/default/tests/integration/test_repos.py
@@ -2,8 +2,8 @@ def test_repos(host):
     if host.system_info.distribution == 'debian':
       repofile = host.file("/etc/apt/sources.list.d/icinga.list")
       assert repofile.is_file
-      assert repofile.user("root")
+      assert repofile.user == "root"
     if host.system_info.distribution == 'centos':
       repofile = host.file("/etc/yum.repos.d/ICINGA-release.repo")
       assert repofile.is_file
-      assert repofile.user("root")
+      assert repofile.user == "root"

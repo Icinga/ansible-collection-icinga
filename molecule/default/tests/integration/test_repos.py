6fa64d9c642b0e368681a92afd49483ef2ac070d
Thilo Wening <thilo.wening@netways.de>
2021-11-11 10:28:25 +0100

    removed repofile is file

--- molecule/default/tests/integration/test_repos.py
+++ molecule/default/tests/integration/test_repos.py
@@ -3,8 +3,7 @@ def test_repos(host):
       file = '/etc/apt/sources.list.d/icinga'
     if host.system_info.distribution == 'centos':
       file = '/etc/yum.repos.d/ICINGA-release'
-    
+
 
     repofile = host.file(file)
     assert repofile.exists
-    assert repofile.is_file

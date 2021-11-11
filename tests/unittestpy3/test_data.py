42ef9410634b88d25d4e3172e12cd87729f4f9e9
Thilo W <mkayontour@gmail.com>
2021-11-11 08:50:53 +0100

    comment print statements

--- tests/unittestpy3/test_data.py
+++ tests/unittestpy3/test_data.py
@@ -10,7 +10,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested boolean true: " + result)
+        #print("Tested boolean true: " + result)
         self.assertEqual(result, 'test = true\n')
 
     def test_false(self):
@@ -20,7 +20,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested boolean false: " + result)
+        #print("Tested boolean false: " + result)
         self.assertEqual(result, 'test = false\n')
 
     def test_string(self):
@@ -30,7 +30,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested string: " + result)
+        #print("Tested string: " + result)
         self.assertEqual(result, 'test = "string"\n')
 
     def test_array(self):
@@ -40,7 +40,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested array: " + result)
+        #print("Tested array: " + result)
         self.assertEqual(result, 'test = [ "item1", "item2", NodeName, ]\n')
 
     def test_constants(self):
@@ -50,7 +50,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested constants: " + result)
+        #print("Tested constants: " + result)
         self.assertEqual(result, 'test = NodeName\n')
 
     def test_dict(self):
@@ -63,7 +63,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested dictionaries: " + result)
+        #print("Tested dictionaries: " + result)
         self.assertMultiLineEqual(result, 'test = {\n  item1 = "value1"\n  item2 = "value2"\n}\n')
 
     def test_unparsed(self):
@@ -73,7 +73,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested Unparsed: " + result)
+        #print("Tested Unparsed: " + result)
         self.assertMultiLineEqual(result, 'test = "test NodeName"\n')
 
     def test_function(self):
@@ -83,7 +83,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested Functions: " + result)
+        #print("Tested Functions: " + result)
         self.assertMultiLineEqual(result, 'test = match("*prod-sfo*", host.name)\n')
 
     def test_rocketsyntax_plus(self):
@@ -93,7 +93,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested rocketsyntax plus: " + result)
+        #print("Tested rocketsyntax plus: " + result)
         self.assertMultiLineEqual(result, 'test += config\n')
 
     def test_rocketsyntax_minus(self):
@@ -103,7 +103,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested rocketsyntax minus: " + result)
+        #print("Tested rocketsyntax minus: " + result)
         self.assertMultiLineEqual(result, 'test -= "string"\n')
 
     def test_rocketsyntax_alt_plus(self):
@@ -113,7 +113,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested rocketsyntax alt plus: " + result)
+        #print("Tested rocketsyntax alt plus: " + result)
         self.assertMultiLineEqual(result, 'test += [ "item1", ZoneName, ]\n')
 
     def test_rocketsyntax_alt_minus(self):
@@ -123,7 +123,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested rocketsyntax alt minus: " + result)
+        #print("Tested rocketsyntax alt minus: " + result)
         self.assertMultiLineEqual(result, 'test -= [ "item1", ZoneName, ]\n')
 
     def test_rocketsyntax_merge_keys(self):
@@ -137,7 +137,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested merge keys: " + result)
+        #print("Tested merge keys: " + result)
         self.assertMultiLineEqual(result, 'vars.item1 = "value1"\nvars.item2 = NodeName\n')
 
     def test_timeintervals(self):
@@ -148,7 +148,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested timeintervals: " + result)
+        #print("Tested timeintervals: " + result)
         self.assertMultiLineEqual(result, 'check_interval = 3m\nwrong_interval = "3f"\n')
 
     def test_numbers(self):
@@ -159,7 +159,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested numbers: " + result)
+        #print("Tested numbers: " + result)
         self.assertMultiLineEqual(result, 'num1 = 3\nnum2 = 2.5\n')
 
     def test_cust_func(self):
@@ -169,7 +169,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested custom functions: " + result)
+        #print("Tested custom functions: " + result)
         self.assertMultiLineEqual(result, 'func = {{  function_blubber(param1,NodeName)  }}\n')
 
     def test_math(self):
@@ -179,7 +179,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested math: " + result)
+        #print("Tested math: " + result)
         self.assertMultiLineEqual(result, 'math_is_fun = 3 * ("value1" -  "value2") / 2\n')
 
     def test_assign_rules(self):
@@ -189,7 +189,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested assign rule: " + result)
+        #print("Tested assign rule: " + result)
         self.assertMultiLineEqual(result, 'assign where match("hostname.fqdn.com", host.name)\n')
 
     def test_ignore_rules(self):
@@ -199,7 +199,7 @@ class TestIcingaParser(unittest.TestCase):
         }
         constants = ["NodeName", "ZoneName", "config"]
         result = ip.parse(attrs=data, constants=constants)
-        print("Tested assign rule: " + result)
+        #print("Tested assign rule: " + result)
         self.assertMultiLineEqual(result, 'ignore where match("hostname.fqdn.com", host.name)\n')
 
 

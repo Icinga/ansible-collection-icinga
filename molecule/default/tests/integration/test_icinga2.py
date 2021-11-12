c038a17518dbed1a4ad15fee1556f38f4c324e9e
Thilo W <mkayontour@gmail.com>
2021-11-12 07:23:02 +0100

    updated tests with services

--- molecule/default/tests/integration/test_icinga2.py
+++ molecule/default/tests/integration/test_icinga2.py
@@ -17,7 +17,10 @@ def test_icinga2_objects(host):
     assert i2_oh.is_directory
     i2_oagent = host.file("/etc/icinga2/zones.d/main/hosts/agent.localdomain.conf")
     assert i2_oagent.is_file
-
+    print i2_oservice.content_string
+    i2_oservice = host.file("/etc/icinga2/zones.d/main/services/services.conf")
+    assert i2_oservice.is_file
+    print i2_oservice.content_string
 
 def test_icinga2_dir(host):
     icinga2_dir = host.file("/etc/icinga2")

f5f3730ce1c3c7770452ba1e01ebbb38bb76c87d
Thilo W <mkayontour@gmail.com>
2021-11-12 10:30:48 +0100

    added user and updated tests

--- molecule/default/tests/integration/test_icinga2.py
+++ molecule/default/tests/integration/test_icinga2.py
@@ -12,27 +12,105 @@ def test_icinga2_configdir(host):
       assert icinga2_cdir.user == "nagios"
       assert icinga2_cdir.group == "nagios"
 
-def test_icinga2_objects(host):
-    i2_oh = host.file("/etc/icinga2/zones.d/main/hosts")
-    assert i2_oh.is_directory
-    i2_oagent = host.file("/etc/icinga2/zones.d/main/hosts/agent.localdomain.conf")
-    assert i2_oagent.is_file
-    print(i2_oagent.content_string)
-    i2_oservice = host.file("/etc/icinga2/zones.d/main/services/services.conf")
-    assert i2_oservice.is_file
-    print(i2_oservice.content_string)
+def test_icinga2_zones_dir(host):
+    i2_zones_dir = host.file("/etc/icinga2/zones.d/main/hosts")
+    if host.system_info.distribution == 'centos':
+      assert i2_zones_dir.is_directory
+      assert i2_zones_dir.user == "icinga"
+      assert i2_zones_dir.group == "icinga"
+    if host.system_info.distribution == 'debian':
+      assert i2_zones_dir.is_directory
+      assert i2_zones_dir.user == "nagios"
+      assert i2_zones_dir.group == "nagios"
+
+def test_icinga2_object_host(host):
+    i2_file = host.file("/etc/icinga2/zones.d/main/hosts/agent.localdomain.conf")
+    print(i2_file.content_string)
+    assert i2_file.is_file
+    assert i2_file.contains('object Zone "agent.localdomain" {')
+    assert i2_file.contains('object Endpoint "agent.localdomain" {')
+    assert i2_file.contains('object Host "agent.localdomain" {')
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "icinga"
+      assert i2_file.group == "icinga"
+      assert i2_file.mode == 0o644
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "nagios"
+      assert i2_file.group == "nagios"
+      assert i2_file.mode == 0o644
+
+def test_icinga2_object_service(host):
+    i2_file = host.file("/etc/icinga2/zones.d/main/services/services.conf")
+    print(i2_file.content_string)
+    assert i2_file.is_file
+    assert i2_file.contains('apply Service "ping" {')
+    assert i2_file.contains('template Service "generic-service" {')
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "icinga"
+      assert i2_file.group == "icinga"
+      assert i2_file.mode == 0o644
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "nagios"
+      assert i2_file.group == "nagios"
+      assert i2_file.mode == 0o644
+
+def test_icinga2_certificate(host):
+    i2_file = host.file("/var/lib/icinga2/certs/icinga-default.crt")
+    assert i2_file.is_file
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "icinga"
+      assert i2_file.group == "icinga"
+      assert i2_file.mode == 0o644
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "nagios"
+      assert i2_file.group == "nagios"
+      assert i2_file.mode == 0o644
+
+def test_icinga2_crt_key(host):
+    i2_file = host.file("/var/lib/icinga2/certs/icinga-default.key")
+    assert i2_file.is_file
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "icinga"
+      assert i2_file.group == "icinga"
+      assert i2_file.mode == 0o600
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "nagios"
+      assert i2_file.group == "nagios"
+      assert i2_file.mode == 0o600
 
 def test_icinga2_dir(host):
     icinga2_dir = host.file("/etc/icinga2")
     assert icinga2_dir.is_directory
 
-def test_icinga2_features(host):
-    f_checker = host.file("/etc/icinga2/features-enabled/checker.conf")
-    f_mainlog = host.file("/etc/icinga2/features-enabled/mainlog.conf")
-    f_api = host.file("/etc/icinga2/features-enabled/api.conf")
-    assert f_checker.exists
-    assert f_checker.linked_to == "/etc/icinga2/features-available/checker.conf"
-    assert f_mainlog.exists
-    assert f_mainlog.linked_to == "/etc/icinga2/features-available/mainlog.conf"
-    assert f_api.exists
-    assert f_api.linked_to == "/etc/icinga2/features-available/api.conf"
+def test_icinga2_feature_checker(host):
+    i2_file = host.file("/etc/icinga2/features-enabled/checker.conf")
+    assert i2_file.exists
+    assert i2_file.linked_to == "/etc/icinga2/features-available/checker.conf"
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "root"
+      assert i2_file.group == "root"
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "root"
+      assert i2_file.group == "root"
+
+def test_icinga2_feature_mainlog(host):
+    i2_file = host.file("/etc/icinga2/features-enabled/mainlog.conf")
+    assert i2_file.exists
+    assert i2_file.linked_to == "/etc/icinga2/features-available/mainlog.conf"
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "root"
+      assert i2_file.group == "root"
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "root"
+      assert i2_file.group == "root"
+
+def test_icinga2_feature_api(host):
+    i2_file = host.file("/etc/icinga2/features-enabled/api.conf")
+    assert i2_file.exists
+    assert i2_file.linked_to == "/etc/icinga2/features-available/api.conf"
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "root"
+      assert i2_file.group == "root"
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "root"
+      assert i2_file.group == "root"

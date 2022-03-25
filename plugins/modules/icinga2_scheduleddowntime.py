83fb42c95d67789a7672bb72a57493ff4f3427a8
Thilo W <mkayontour@users.noreply.github.com>
2022-03-25 10:40:24 +0100

    Cleanup Metadata and Flake8 linting warnings (#92)
    * add WA for Galaxy issue #2438
    
    * fix metadata for galay.yml
    
    * fixed indentations and whitespaces from flake8
    
    * cleanup flake8 warnings
    
    * cleanup flake8 warnings
    
    * cleanup flake8 warnings
    
    * added author information
    
    * fix jinja2 for dependency problems

--- plugins/modules/icinga2_scheduleddowntime.py
+++ plugins/modules/icinga2_scheduleddowntime.py
@@ -1,31 +1,29 @@
-#!/usr/bin/python
-
-
 from ansible.module_utils.basic import AnsibleModule
+
 
 def main():
     module = AnsibleModule(
         supports_check_mode=True,
-        argument_spec = dict(
-            state         = dict(default='present', choices=['present', 'absent']),
-            name          = dict(required=True),
-            order         = dict(default=10, type='int'),
-            file          = dict(required=True, type='str'),
-            template      = dict(default=False, type='bool'),
-            imports       = dict(default=list(), type='list', elements='str'),
-            apply         = dict(default=False, type='bool'),
-            apply_target  = dict(type='str', choices=['Host', 'Service']),
-            display_name  = dict(type='str'),
-            host_name     = dict(type='str'),
-            service_name  = dict(type='str'),
-            author        = dict(type='str'),
-            comment       = dict(type='str'),
-            fixed         = dict(type='bool'),
-            duration      = dict(type='str'),
-            ranges        = dict(type='dict'),
-            child_options = dict(type='str'),
-            assign        = dict(default=list(), type='list', elements='str'),
-			ignore        = dict(default=list(), type='list', elements='str'),
+        argument_spec=dict(
+            state=dict(default='present', choices=['present', 'absent']),
+            name=dict(required=True),
+            order=dict(default=10, type='int'),
+            file=dict(required=True, type='str'),
+            template=dict(default=False, type='bool'),
+            imports=dict(default=list(), type='list', elements='str'),
+            apply=dict(default=False, type='bool'),
+            apply_target=dict(type='str', choices=['Host', 'Service']),
+            display_name=dict(type='str'),
+            host_name=dict(type='str'),
+            service_name=dict(type='str'),
+            author=dict(type='str'),
+            comment=dict(type='str'),
+            fixed=dict(type='bool'),
+            duration=dict(type='str'),
+            ranges=dict(type='dict'),
+            child_options=dict(type='str'),
+            assign=dict(default=list(), type='list', elements='str'),
+            ignore=dict(default=list(), type='list', elements='str'),
         )
     )
 
@@ -39,7 +37,19 @@ def main():
     apply = args.pop('apply')
     apply_target = args.pop('apply_target')
 
-    module.exit_json(changed=False, args=args, name=name, order=str(order), state=state, file=file, template=template, imports=imports, apply=apply, apply_target=apply_target)
+    module.exit_json(
+        changed=False,
+        args=args,
+        name=name,
+        order=str(order),
+        state=state,
+        file=file,
+        template=template,
+        imports=imports,
+        apply=apply,
+        apply_target=apply_target
+    )
+
 
 if __name__ == '__main__':
     main()

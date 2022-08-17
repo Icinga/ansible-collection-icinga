5956a75f065f614825ff25c96d9ff18cb18fb3bb
Thilo W <mkayontour@gmail.com>
2022-08-17 15:14:42 +0200

    add tests for perfdata

--- molecule/default/tests/integration/test_icinga2.py
+++ molecule/default/tests/integration/test_icinga2.py
@@ -168,3 +168,16 @@ def test_icinga2_feature_gelf(host):
     if host.system_info.distribution == 'debian':
       assert i2_file.user == "nagios"
       assert i2_file.group == "nagios"
+
+def test_icinga2_feature_perfdata(host):
+    i2_file = host.file("/etc/icinga2/features-available/perfdata.conf")
+    i2_link = host.file("/etc/icinga2/features-enabled/perfdata.conf")
+    assert i2_file.exists
+    assert i2_file.contains('object GelfWriter "perfdata" {')
+    assert i2_link.linked_to == "/etc/icinga2/features-available/perfdata.conf"
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "icinga"
+      assert i2_file.group == "icinga"
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "nagios"
+      assert i2_file.group == "nagios"

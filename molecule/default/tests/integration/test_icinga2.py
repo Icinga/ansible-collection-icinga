e5517215394c4166cf0f449bad87f8d002593913
Afeef Ghannam <39904920+afeefghannam89@users.noreply.github.com>
2022-08-17 10:31:35 +0200

    Add object and feature for ElasticsearchWriter (#66)
    * add feature module 
    
    * add Tests for Feature 
    
    * add Documentation 
    
    * Closes Feature #13
    
    Co-authored-by: Thilo W <mkayontour@gmail.com>

--- molecule/default/tests/integration/test_icinga2.py
+++ molecule/default/tests/integration/test_icinga2.py
@@ -142,3 +142,16 @@ def test_icinga2_feature_opentsdb(host):
     if host.system_info.distribution == 'debian':
       assert i2_file.user == "nagios"
       assert i2_file.group == "nagios"
+
+def test_icinga2_feature_elasticsearch(host):
+    i2_file = host.file("/etc/icinga2/features-available/elasticsearch.conf")
+    i2_link = host.file("/etc/icinga2/features-enabled/elasticsearch.conf")
+    assert i2_file.exists
+    assert i2_file.contains('object ElasticsearchWriter "elasticsearch" {')
+    assert i2_link.linked_to == "/etc/icinga2/features-available/elasticsearch.conf"
+    if host.system_info.distribution == 'centos':
+      assert i2_file.user == "icinga"
+      assert i2_file.group == "icinga"
+    if host.system_info.distribution == 'debian':
+      assert i2_file.user == "nagios"
+      assert i2_file.group == "nagios"

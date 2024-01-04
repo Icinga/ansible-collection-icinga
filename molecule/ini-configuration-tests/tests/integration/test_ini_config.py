cf8fb04301a7959abf05a3d71179d8bf46d4c533
Thilo W <mkayontour@gmail.com>
2024-01-04 12:26:23 +0100

    updated tests and fixed versions

--- molecule/ini-configuration-tests/tests/integration/test_ini_config.py
+++ molecule/ini-configuration-tests/tests/integration/test_ini_config.py
@@ -2,28 +2,28 @@ def test_string(host):
     i2_file = host.file("/tmp/string")
     print(i2_file.content_string)
     assert i2_file.is_file
-    assert i2_file.content_string == "\n[section]\ntest = string"
+    assert i2_file.content_string == "\n[section]\ntest = string\n"
 
 def test_number(host):
     i2_file = host.file("/tmp/number")
     print(i2_file.content_string)
     assert i2_file.is_file
-    assert i2_file.content_string == '\n[section]\ntest = "10"'
+    assert i2_file.content_string == '\n[section]\ntest = "10"\n'
 
 def test_list(host):
     i2_file = host.file("/tmp/list")
     print(i2_file.content_string)
     assert i2_file.is_file
-    assert i2_file.content_string == '\n[section]\ntest = "foo, bar, baz"'
+    assert i2_file.content_string == '\n[section]\ntest = "foo, bar, baz"\n'
 
 def test_advanced_filter(host):
     i2_file = host.file("/tmp/advanced_filter")
     print(i2_file.content_string)
     assert i2_file.is_file
-    assert i2_file.content_string == "\n[section]\ntest = '!(objectClass=user)'\ntest2 = '!(objectClass=user)'\ntest3 = '!attribute'"
+    assert i2_file.content_string == "\n[section]\ntest = '!(objectClass=user)'\ntest2 = '!(objectClass=user)'\ntest3 = '!attribute'\n"
 
 def test_equal_sign(host):
     i2_file = host.file("/tmp/equal_sign")
     print(i2_file.content_string)
     assert i2_file.is_file
-    assert i2_file.content_string == "\n[section]\ntest = 'equal=sign'"
+    assert i2_file.content_string == "\n[section]\ntest = 'equal=sign'\n"

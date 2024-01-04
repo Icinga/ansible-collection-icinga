e313cc487e90cb4bcca855fe4ae8ccb3e048d2aa
Thilo W <mkayontour@gmail.com>
2024-01-04 18:11:31 +0100

    added test for default build

---
+++ molecule/default/tests/integration/test_icingadb_retentions.py
@@ -0,0 +1,13 @@
+# Vars in host_vars
+
+def test_icingadb_config(host):
+    i2_file = host.file("/etc/icingadb/config.yml")
+    assert i2_file.is_file
+    assert i2_file.contains('  history-days: 10')
+    assert i2_file.contains('  sla-days: 11')
+    assert i2_file.contains('    acknowledgement: 20')
+    assert i2_file.contains('    comment: 30')
+    assert i2_file.contains('    state: 60')
+    assert i2_file.contains('    downtime: 10')
+    assert i2_file.contains('    notification: 4')
+    assert i2_file.contains('    flapping: 2')

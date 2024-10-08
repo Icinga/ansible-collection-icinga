bcaece3ed66fa1961b81f8be593a34ea7273f685
Donien <88634789+Donien@users.noreply.github.com>
2024-10-08 10:50:16 +0200

    Add documentation to module 'checkcommand'

--- plugins/modules/icinga2_checkcommand.py
+++ plugins/modules/icinga2_checkcommand.py
@@ -1,7 +1,133 @@
 from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = '''
-    name: icinga2_checkcommand
+name: icinga2_checkcommand
+short_description: Creates information for CheckCommand object.
+description:
+  - Returns information used to create a CheckCommand object.
+version_added: 0.1.0
+author:
+  - Lennart Betz <lennart.betz@netways.de>
+
+options:
+  name:
+    description:
+      - The name of the CheckCommand object.
+    required: true
+    type: str
+  state:
+    description:
+      - The state of the CheckCommand object.
+    required: false
+    default: present
+    choices: [ "present", "absent" ]
+    type: str
+  order:
+    description:
+      - Value to determine internal precedence.
+    required: false
+    default: 10
+    type: int
+  file:
+    description:
+      - Path to the file in which the object will be defined.
+    required: true
+    type: str
+  command:
+    description:
+      - The command. This is an array of individual command arguments.
+    required: true
+    type: list
+    elements: str
+  env:
+    description:
+      - A dictionary of macros which should be exported as environment variables prior to executing the command.
+    required: false
+    type: dict
+  vars:
+    description:
+      - A dictionary containing custom variables that are specific to this command.
+    required: false
+    type: dict
+  timeout:
+    description:
+      - The command timeout in seconds. Defaults to 1m.
+    required: false
+    type: str
+  arguments:
+    description:
+      - A dictionary of command arguments.
+    required: false
+    type: dict
+'''
+
+EXAMPLES = '''
+icinga.icinga.icinga2_checkcommand:
+  name: "mycheckcommand"
+  command:
+    - /usr/lib64/nagios/plugins
+    - /contrib
+    - /check_nwc_health
+'''
+
+RETURN = '''
+args:
+  description: Arguments used to create the CheckCommand object.
+  returned: success
+  type: dict
+  contains:
+    arguments:
+      description: The arguments passed.
+      returned: success
+      type: dict
+      sample: null
+    command:
+      description: The strings that make up the command.
+      returned: success
+      type: list
+      elements: str
+      sample: [ "/usr/lib64/nagios/plugins", "/contrib", "/check_nwc_health" ]
+    env:
+      description: The specified macros.
+      returned: success
+      type: dict
+      sample: null
+    timeout:
+      description: The specified timeout.
+      returned: success
+      type: str
+      sample: 15s
+imports:
+  description: Import statements made for the CheckCommand object.
+  returned: success
+  type: list
+  elements: str
+  sample: [ "default-template", "ssh-wrapper" ]
+template:
+  description: Whether the CheckCommand is set to be a template.
+  returned: success
+  type: bool
+  sample: false
+file:
+  description: Path to the file that will contain the object.
+  returned: success
+  type: str
+  sample: custom/checkcommands.conf
+name:
+  description: The name of the CheckCommand object.
+  returned: success
+  type: str
+  sample: mycheckcommand
+order:
+  description: The order value of this object. Used internally when combining multiple templates / objects.
+  returned: success
+  type: int
+  sample: 10
+state:
+  description: The chosen state for the object.
+  returned: success
+  type: str
+  sample: present
 '''
 
 def main():

fa06a7092b1c28c230cb881def54b7b0c24f3f62
Donien <88634789+Donien@users.noreply.github.com>
2024-10-11 12:06:40 +0200

    Add documentation to module 'externalcommandlistener'

--- plugins/modules/icinga2_externalcommandlistener.py
+++ plugins/modules/icinga2_externalcommandlistener.py
@@ -1,7 +1,83 @@
 from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = '''
-    name: icinga2_externalcommandlistener
+name: icinga2_externalcommandlistener
+short_description: Creates information for ExternalCommandListener object.
+description:
+  - Returns information used to create an ExternalCommandListener object.
+version_added: 0.1.0
+author:
+  - Lennart Betz <lennart.betz@netways.de>
+
+options:
+  name:
+    description:
+      - The name of the ExternalCommandListener object.
+    required: true
+    type: str
+  state:
+    description:
+      - The state of the ExternalCommandListener object.
+    required: false
+    default: present
+    choices: [ "present", "absent" ]
+    type: str
+  order:
+    description:
+      - Value to determine internal precedence.
+    required: false
+    default: 10
+    type: int
+  file:
+    description:
+      - Path to the file in which the object will be defined.
+    required: false
+    default: "features-available/command.conf"
+    type: str
+  command_path:
+    description:
+      - Path to the command pipe.
+    required: false
+    default: "features-available/command.conf"
+    type: str
+'''
+
+EXAMPLES = '''
+icinga.icinga.icinga2_externalcommandlistener:
+  name: "myexternalcommandlistener"
+'''
+
+RETURN = '''
+args:
+  description: Arguments used to create the ExternalCommandListener object.
+  returned: success
+  type: dict
+  contains:
+    command_path:
+      description: The specified path to the pipe.
+      returned: success
+      type: str
+      sample: null
+file:
+  description: Path to the file that will contain the object.
+  returned: success
+  type: str
+  sample: features-available/command.conf
+name:
+  description: The name of the ExternalCommandListener object.
+  returned: success
+  type: str
+  sample: myexternalcommandlistener
+order:
+  description: The order value of this object. Used internally when combining multiple templates / objects.
+  returned: success
+  type: int
+  sample: 10
+state:
+  description: The chosen state for the object.
+  returned: success
+  type: str
+  sample: present
 '''
 
 def main():

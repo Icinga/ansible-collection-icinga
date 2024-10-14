d443db652a1ba6b625bb4e9f950bc3c554e67364
Donien <88634789+Donien@users.noreply.github.com>
2024-10-14 12:33:18 +0200

    Add documentation to module 'notificationcomponent'

--- plugins/modules/icinga2_notificationcomponent.py
+++ plugins/modules/icinga2_notificationcomponent.py
@@ -1,7 +1,84 @@
 from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = '''
-    name: icinga2_notificationcomponent
+name: icinga2_notificationcomponent
+short_description: Creates information for NotificationComponent object.
+description:
+  - Returns information used to create a NotificationComponent object.
+version_added: 0.1.0
+author:
+  - Lennart Betz <lennart.betz@netways.de>
+
+options:
+  name:
+    description:
+      - The name of the NotificationComponent object.
+    required: true
+    type: str
+  state:
+    description:
+      - The state of the NotificationComponent object.
+    required: false
+    default: present
+    choices: [ "present", "absent" ]
+    type: str
+  order:
+    description:
+      - Value to determine internal precedence.
+    required: false
+    default: 10
+    type: int
+  file:
+    description:
+      - Path to the file in which the object will be defined.
+    required: false
+    default: "features-available/notification.conf"
+    type: str
+  enable_ha:
+    description:
+      - Enable the high availability functionality. Only valid in a cluster setup.
+      - Disabling this currently only affects reminder notifications.
+    required: false
+    type: bool
+'''
+
+EXAMPLES = '''
+icinga.icinga.icinga2_notificationcomponent:
+  name: "mynotificationcomponent"
+  enable_ha: true
+'''
+
+RETURN = '''
+args:
+  description: Arguments used to create the NotificationComponent object.
+  returned: success
+  type: dict
+  contains:
+    enable_ha:
+      description: Whether HA is enabled.
+      returned: success
+      type: bool
+      sample: true
+file:
+  description: Path to the file that will contain the object.
+  returned: success
+  type: str
+  sample: features-available/notification.conf
+name:
+  description: The name of the NotificationComponent object.
+  returned: success
+  type: str
+  sample: mynotificationcomponent
+order:
+  description: The order value of this object. Used internally when combining multiple templates / objects.
+  returned: success
+  type: int
+  sample: 10
+state:
+  description: The chosen state for the object.
+  returned: success
+  type: str
+  sample: present
 '''
 
 def main():

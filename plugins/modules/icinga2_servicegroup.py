e1f679645e281ca79e86f7375c7e23fe3c34ce9b
Donien <88634789+Donien@users.noreply.github.com>
2024-12-05 14:22:52 +0100

    Add documentation to module 'servicegroup'

--- plugins/modules/icinga2_servicegroup.py
+++ plugins/modules/icinga2_servicegroup.py
@@ -1,7 +1,128 @@
 from ansible.module_utils.basic import AnsibleModule
 
 DOCUMENTATION = '''
-    name: icinga2_servicegroup
+name: icinga2_servicegroup
+short_description: Creates information for ServiceGroup object.
+description:
+  - Returns information used to create a ServiceGroup object.
+version_added: 0.1.0
+author:
+  - Lennart Betz <lennart.betz@netways.de>
+
+options:
+  name:
+    description:
+      - The name of the ServiceGroup object.
+    required: true
+    type: str
+  state:
+    description:
+      - The state of the ServiceGroup object.
+    required: false
+    default: present
+    choices: [ "present", "absent" ]
+    type: str
+  order:
+    description:
+      - Value to determine internal precedence.
+    required: false
+    default: 10
+    type: int
+  file:
+    description:
+      - Path to the file in which the object will be defined.
+    required: true
+    type: str
+  display_name:
+    description:
+      - A short description of the ServiceGroup.
+    required: false
+    type: str
+  groups:
+    description:
+      - An array of nested group names.
+    required: false
+    default: []
+    type: list
+    elements: str
+  assign:
+    description:
+      - List of conditions for where to apply this ServiceGroup.
+    required: false
+    default: []
+    type: list
+    elements: str
+  ignore:
+    description:
+      - List of conditions for where to ignore this ServiceGroup.
+    required: false
+    default: []
+    type: list
+    elements: str
+'''
+
+EXAMPLES = '''
+- icinga.icinga.icinga2_servicegroup:
+    name: "myservicegroup"
+    file: "custom/servicegroup.conf"
+    display_name: "My ServiceGroup Display Name"
+    assign:
+      - host.vars.os == Linux
+'''
+
+RETURN = '''
+args:
+  description: Arguments used to create the ServiceGroup object.
+  returned: success
+  type: dict
+  contains:
+    assign:
+      description: The specified assign rule.
+      returned: success
+      type: list
+      elements: str
+      sample:
+        - host.vars.os == linux
+    display_name:
+      description: The specified display name.
+      returned: success
+      type: str
+      sample: My ServiceGroup Display Name
+    groups:
+      description: The specified groups.
+      returned: success
+      type: list
+      elements: str
+      sample:
+        - group1
+        - group2
+    ignore:
+      description: The specified ignore rule.
+      returned: success
+      type: list
+      elements: str
+      sample:
+        - host.vars.os == windows
+file:
+  description: Path to the file that will contain the object.
+  returned: success
+  type: str
+  sample: custom/servicegroup.conf
+name:
+  description: The name of the ServiceGroup object.
+  returned: success
+  type: str
+  sample: myservicegroup
+order:
+  description: The order value of this object. Used internally when combining multiple templates / objects.
+  returned: success
+  type: int
+  sample: 10
+state:
+  description: The chosen state for the object.
+  returned: success
+  type: str
+  sample: present
 '''
 
 def main():
